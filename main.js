1b392970dfc6a624c9959f0923eca4c21013f007
M.YOUSUF AMIN <m.yousuf8768@gmail.com>
2024-09-15 18:01:14 +0500

    add apiUrl and apiKey

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-const data = 0;
+const apiKey = "105b3449e54997efc78dbad890bf50dc";
+const defaultApiUrl = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=1`;
+const searchApiUrl = `https://api.themoviedb.org/3/search/movie?&api_key=${apiKey}&query=avengers`;
+const imageUrl = `https://image.tmdb.org/t/p/w1280`
